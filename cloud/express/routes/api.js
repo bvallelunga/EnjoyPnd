969cad8006ff33a81d16ab99b5088d60b67b3a88
nelson <nelsonr462@gmail.com>
2015-01-10 04:16:03 -0500

    fixed api.post route again

--- cloud/express/routes/api.js
+++ cloud/express/routes/api.js
@@ -31,3 +31,4 @@ module.exports.auth = function(req, res, next) {
 }
 
 module.exports.get = require("cloud/express/routes/api.get.js")
+module.exports.get = require("cloud/express/routes/api.post.js")
