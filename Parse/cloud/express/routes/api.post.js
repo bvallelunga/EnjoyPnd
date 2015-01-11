d56e10774de2e10790983e9314208c2e451ae308
etkanesh <Eric.T.Kaneshige@gmail.com>
2015-01-10 20:06:27 -0500

    added webhook and handling worker applicants

--- Parse/cloud/express/routes/api.post.js
+++ Parse/cloud/express/routes/api.post.js
@@ -1,14 +1,15 @@
 var Jobs = Parse.Object.extend("Jobs")
-var Company = Parse.Object.extend("Company")
 
 module.exports.pending = function(req, res) {
-var company = req.company
-var workers = company.relation("workers")
-var pendingWorkers = company.relation("pendingWorkers")
+  var company = req.company
+  var workers = company.relation("workers")
+  var pendingWorkers = company.relation("pendingWorkers")
 
-var query = new Parse.Query(Parse.User)
-//user? loop? look at all users? does it cycle through? for later implementation
-query.get(req.param("user"), function(user) {
+  var query = new Parse.Query(Parse.User)
+  //user? loop? look at all users? does it cycle through? for later implementation
+
+
+  query.get(req.param("user"), function(user) {
   pendingWorkers.remove(user)
 
   if(req.param("accepted") == "true") {
@@ -44,12 +45,11 @@ module.exports.jobs = function(req, res) {
   job.set("company", company)
   job.set("destination", destination.address)
   job.set("name", req.param("name"))
-  job.set("status", 1)
   job.set("pickup", pickup.address)
-  job.set("pickupGeo", new Parse.GeoPoint(parseFloat(pickup.lat), parseFloat( pickup.lng)))
+  job.set("pickupGeo", new Parse.GeoPoint(pickup.lat, pickup.lng))
 
-  if(!destination || !pickup || req.param("name") == null) {
-    return res.errorT("Missing parameter(s)")
+  if(!destination || !name || !pickup || !pickupGeo) {
+    res.errorT("Missing parameter(s)")
   }
 
   job.save().then(function() {
@@ -57,15 +57,17 @@ module.exports.jobs = function(req, res) {
   }, res.errorT)
 }
 
+var Job = Parse.Object.extend("Jobs")
 
-module.exports.cancel = function(req, res) {  
-  var job = req.param("job")
-  var query = new Parse.Query(Jobs)
-
-  query.get(job, function(job) {
+module.exports.cancel = function(req, res) {
+  var company = req.company
+  
+  var query = new Parse.Query(Job)
+  query.get(req.param("job"), function(job) {
       job.set("status", 5)
-      return job.save()
-  }).then(function() {
+  })
+  
+  job.save().then(function() {
     res.successT()
   }, res.errorT)
 }
--- Parse/cloud/functions/jobs.js
+++ Parse/cloud/functions/jobs.js
@@ -1,8 +1,10 @@
-Parse.Cloud.define("jobs.cancelled", function(req, res) {
+Parse.Cloud.define("jobs_Cancelled", function(req, res) { //requires the list of companies
+console.log('%s', "start cancellation") //message confirmation
   var company = req.params.company
   var webhook = company.get("webhook")
 
   if(!webhook) {
+    console.log('%s', "no webhook") //message confirmation
     return res.successT()
   }
 
@@ -13,10 +15,85 @@ Parse.Cloud.define("jobs.cancelled", function(req, res) {
       success: true,
       type: "job.cancelled",
       reason: "Carrier aborted during route"
-    }
-  }).then(function(response) {
-    res.success()
+    } 
+  } 
+  ).then(function(response) {
+    res.successT()
+    console.log('%s', "job cancelled success") //message confirmation
+
   }, function(error) {
     res.errorT(error)
+console.log('%s', "job cancelled failed") //message failure
+
+  })
+})
+
+
+Parse.Cloud.define("jobs_approved", function(req, res) {
+  var company = req.params.company
+  var webhook = company.get("webhook")
+  if (!webhook) {
+    return res.successT()
+  }
+  Parse.Cloud.httpRequest ({
+    url: webhook,
+    method: "post",
+    data: {
+      success: true,
+      type: "job.approved",
+      reason: "Carrier accepted job"
+    }
+  }),then(function(response){
+    res.successT()
+    console.log('%s', "job approved success") //message confirmation
+  }, function(error) {
+     res.errorT(error)
+     console.log('%s', "job approved failed") //message failure
+  })
+})
+
+Parse.Cloud.define("jobs_enroute", function(req, res) {
+  var company = req.params.company
+  var webhook = company.get("webhook")
+  if (!webhook) {
+    return res.successT()
+  }
+  Parse.Cloud.httpRequest ({
+    url: webhook,
+    method: "post",
+    data: {
+      success: true,
+      type: "job.enroute",
+      reason: "Carrier is in route"
+    }
+  }),then(function(response){
+    res.successT()
+    console.log('%s', "job enroute confirmed") //message confirmation
+  }, function(error) {
+     res.errorT(error)
+     console.log('%s', "job enroute failed confirmation") //message failure
+  })
+})
+
+Parse.Cloud.define("jobs_pending", function(req, res) {
+  var company = req.params.company
+  var webhook = company.get("webhook")
+  if (!webhook) {
+    return res.successT()
+  }
+  Parse.Cloud.httpRequest ({
+    url: webhook,
+    method: "post",
+    data: {
+      success: true,
+      type: "job.pending",
+      reason: "No Carrier has accepted this job yet."
+    }
+  }),then(function(response){
+    res.successT()
+    console.log('%s', "job pending confirmed") //message confirmation
+  }, function(error) {
+     res.errorT(error)
+     console.log('%s', "job pending failed confirmation") //message failure
   })
 })
