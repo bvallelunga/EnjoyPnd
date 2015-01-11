4e0ea6095f536ec8b1d9c2d95013e361fb4262dc
Brian Vallelunga <vallelungabrian@gmail.com>
2015-01-10 20:23:16 -0800

    got map page working

--- iOS/EnjoyPnD/MapViewController.swift
+++ iOS/EnjoyPnD/MapViewController.swift
@@ -6,13 +6,47 @@
 //  Copyright (c) 2015 Brian Vallelunga. All rights reserved.
 //
 
-class MapViewController: UIViewController {
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationLabel: UILabel!
     
     // MARK: Instance Variables
     private var user: User = User.current()
-    @IBOutlet weak var mapView: MKMapView!
+    private var locationManager: CLLocationManager!
     
     // MARK: UIViewController Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create Location Manager
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.activityType = .Fitness
+        
+        if (self.locationManager.respondsToSelector(Selector("requestAlwaysAuthorization"))) {
+            self.locationManager.requestAlwaysAuthorization()
+        }
+        
+        self.locationManager.startUpdatingLocation()
+        
+        // Configure MapView
+        self.mapView.delegate = self
+        self.mapView.showsUserLocation = true
+        
+        // Configure Location Label
+        var buttonBorder = UIView(frame: CGRectMake(0, self.locationLabel.frame.height-1, self.view.frame.width, 1))
+        buttonBorder.backgroundColor = UIColor(white: 0, alpha: 0.1)
+        self.locationLabel.addSubview(buttonBorder)
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -33,6 +67,28 @@ class MapViewController: UIViewController {
                 NSFontAttributeName: font,
                 NSShadowAttributeName: shadow
             ]
+        }
+    }
+    
+    // MARK: LocationManager Methods
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let firstLocation = locations.first as? CLLocation {
+            let region = MKCoordinateRegionMakeWithDistance(firstLocation.coordinate, 1000, 1000)
+            self.mapView.setCenterCoordinate(firstLocation.coordinate, animated: true)
+            self.mapView.setRegion(region, animated: true)
+            
+            var geoCoder = CLGeocoder()
+            geoCoder.reverseGeocodeLocation(locations.last as CLLocation, completionHandler: { (placeMarks: [AnyObject]!, error: NSError!) -> Void in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if placeMarks != nil && !placeMarks.isEmpty {
+                        var placeMark = placeMarks[0] as CLPlacemark
+                        var lines = placeMark.addressDictionary["FormattedAddressLines"] as? NSArray
+                        self.locationLabel.text = lines?.firstObject as NSString
+                    } else if error != nil {
+                        println(error)
+                    }
+                })
+            })
         }
     }
 
