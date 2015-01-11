1389688c0995d7d927b5b2d6f47e3c8229b884d0
Brian Vallelunga <vallelungabrian@gmail.com>
2015-01-10 17:11:52 -0800

    small fix and ios

--- Parse/cloud/express/routes/api.post.js
+++ Parse/cloud/express/routes/api.post.js
@@ -4,21 +4,19 @@ module.exports.pending = function(req, res) {
   var company = req.company
   var workers = company.relation("workers")
   var pendingWorkers = company.relation("pendingWorkers")
-
   var query = new Parse.Query(Parse.User)
-  //user? loop? look at all users? does it cycle through? for later implementation
-
 
   query.get(req.param("user"), function(user) {
-  pendingWorkers.remove(user)
+    pendingWorkers.remove(user)
 
-  if(req.param("accepted") == "true") {
-    workers.add(user)
-  }
-  company.save().then(function() {
-    res.successT()
-  }, res.errorT)
-})}
+    if(req.param("accepted") == "true") {
+      workers.add(user)
+    }
+    company.save().then(function() {
+      res.successT()
+    }, res.errorT)
+  })
+}
 
 module.exports.jobs = function(req, res) {
   /*
@@ -61,12 +59,12 @@ var Job = Parse.Object.extend("Jobs")
 
 module.exports.cancel = function(req, res) {
   var company = req.company
-  
+
   var query = new Parse.Query(Job)
   query.get(req.param("job"), function(job) {
       job.set("status", 5)
   })
-  
+
   job.save().then(function() {
     res.successT()
   }, res.errorT)
---
+++ iOS/EnjoyPnD/AddInfoController.swift
@@ -0,0 +1,16 @@
+//
+//  AddInfoController.swift
+//  EnjoyPnD
+//
+//  Created by Brian Vallelunga on 1/10/15.
+//  Copyright (c) 2015 Brian Vallelunga. All rights reserved.
+//
+
+class AddInfoController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var descriptionInput: UITextField!
+    
+    //
+}
